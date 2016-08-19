313a31beaa03dc5ff2a6254d968ef7e4cafc463a
Håkan Råberg <hraberg@yahoo.se>
2016-08-19 22:32:41 +0100

    Clojure 1.9.0-alpha11

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/hraberg/sanningens-silverflod"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha11"]
                  [org.clojure/clojurescript "1.9.93" :exclusions [org.clojure/tools.reader]]
                  [org.clojure/core.async "0.2.385"]
                  [datascript "0.15.2"]]
