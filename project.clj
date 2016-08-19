6da36e54714f6914ff2e7269aa332a16bde5c4e4
Håkan Råberg <hraberg@yahoo.se>
2016-08-19 23:57:05 +0100

    Clojurescript 1.9.225

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha11"]
-                 [org.clojure/clojurescript "1.9.93" :exclusions [org.clojure/tools.reader]]
+                 [org.clojure/clojurescript "1.9.225" :exclusions [org.clojure/tools.reader]]
                  [org.clojure/core.async "0.2.385"]
                  [datascript "0.15.2"]]
   :pedantic? :abort
